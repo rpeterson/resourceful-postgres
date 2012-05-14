703e32620177adaaa8a132e929d15cf67b5cd7b0
rpeterson <ryan@mathusee.com>
2012-05-14 10:49:31 -0400

    updateing tabs to spaced

--- index.js
+++ index.js
@@ -6,42 +6,42 @@ var url = require('url'),
 
 var PostgreSQL = resourceful.engines.Pg = function Pg(config) {
 
- 	if(config && config.table) {
-		this.table = config.table;
-	} else {
-		throw new Error('table must be set in the config for each model.')
-	}
+  if(config && config.table) {
+    this.table = config.table;
+  } else {
+    throw new Error('table must be set in the config for each model.')
+  }
 
-	if (config.uri) {
-		var uri = url.parse('tcp://' + config.uri, true);
-		config.host = uri.hostname;
-		config.port = uri.port;
+  if (config.uri) {
+    var uri = url.parse('tcp://' + config.uri, true);
+    config.host = uri.hostname;
+    config.port = uri.port;
 
-		if (uri.pathname) {
-		  config.database = uri.pathname.replace(/^\/|\/$/g, '');
-		}
+    if (uri.pathname) {
+      config.database = uri.pathname.replace(/^\/|\/$/g, '');
+    }
 
-		if (uri.query) {
-		  resourceful.mixin(config, uri.query);
-		}
-	}
+    if (uri.query) {
+      resourceful.mixin(config, uri.query);
+    }
+  }
 
-	config.host     = config.host     || 'localhost';
-	config.auth     = config.auth     || 'postgres';
-	config.port     = config.port     || 5432;
-	config.database = config.database || resourceful.env || 'postgres';
-	
-	config.uri = url.format({
-		protocol: 'tcp',
-		auth: config.auth,
-		hostname: config.host,
-		port: config.port,
-		pathname: '/' + config.database
-	});
+  config.host     = config.host     || 'localhost';
+  config.auth     = config.auth     || 'postgres';
+  config.port     = config.port     || 5432;
+  config.database = config.database || resourceful.env || 'postgres';
+  
+  config.uri = url.format({
+    protocol: 'tcp',
+    auth: config.auth,
+    hostname: config.host,
+    port: config.port,
+    pathname: '/' + config.database
+  });
 
-	config.port = parseInt(config.port, 10);
+  config.port = parseInt(config.port, 10);
 
-	this.client = config.client || new pg.Client(config.uri);
+  this.client = config.client || new pg.Client(config.uri);
 
   this.client.connect();
 
@@ -58,31 +58,31 @@ PostgreSQL.prototype.protocol = 'pg';
 
 
 PostgreSQL.prototype.load = function (data) {
-	throw new(Error)("Load not valid for PostgreSQL engine.");
+  throw new(Error)("Load not valid for PostgreSQL engine.");
 };
 
 
 PostgreSQL.prototype.get = function(id, cb) {
-	this._query(
-		"SELECT * " 
-		+ " FROM " + this.table 
-		+ " WHERE " + this._key + " = " + id 
-		+ " LIMIT 1"
+  this._query(
+    "SELECT * " 
+    + " FROM " + this.table 
+    + " WHERE " + this._key + " = " + id 
+    + " LIMIT 1"
     , cb);
 };
 
 
 PostgreSQL.prototype.save = function(obj, cb) {
-	if( obj.id ) {
+  if( obj.id ) {
     this.update(obj.id, obj, cb);
-	} else {
-		this.create(obj, cb);
-	}
+  } else {
+    this.create(obj, cb);
+  }
 };
 
 
 PostgreSQL.prototype.create = function(obj, cb) {
-	this._query(
+  this._query(
       "INSERT INTO " + this.table 
       + " (" + this._getFields(obj) + ")" 
       + " VALUES (" + this._getValues(obj) + ") RETURNING *;"
@@ -100,19 +100,19 @@ PostgreSQL.prototype.update = function(id, obj, cb) {
 
 
 PostgreSQL.prototype.destroy = function(id, cb) {
-	this._query("DELETE FROM " + this.table 
-		+ " WHERE " + this._key + " = " + id 
-	, cb);
+  this._query("DELETE FROM " + this.table 
+    + " WHERE " + this._key + " = " + id 
+  , cb);
 };
 
 
 PostgreSQL.prototype.find = function(conditions, cb) {
-	//To-Do 
+  //To-Do 
 };
 
 
 PostgreSQL.prototype.filter = function(filter, cb) {
-	//To-Do
+  //To-Do
 };
 
 
